babccff3984ffb00c30d128e71ff9039355b2704
Msiddhesh008 <msiddhesh008@gmail.com>
2022-12-08 23:40:26 +0530

    UI Updated

--- script.js
+++ script.js
@@ -1,91 +1,160 @@
-var noteList = [];
-
 var ul = document.getElementById("list");
-
 var addInput = document.getElementById("add-input");
-
 var addNote = document.getElementById("add-btn");
+var deleteNote = document.getElementsByClassName("close");
+var hide = document.querySelector("#hide-list label");
+var editNote = document.getElementsByClassName("edit-notes");
 
-var date = new Date();
+// var data = document.getElementById("data").innerHTML;
+var typed = new Typed(".auto-type", {
+  //write anything Here
+  strings: ["Welcome To Note Manager.."],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
 
 addNote.addEventListener("click", function (e) {
-  e.preventDefault(); // Preventing page from refreshing by clicking on Submit
+  e.preventDefault();
 
-  noteList.push(addInput.value); // Appends or Add new elements to the end of an array.
+  var li = document.createElement("li");
+  ul.appendChild(li);
 
-  var li = document.createElement("li"); //  Created an instance of the element for the specified tag.
-  ul.appendChild(li); // Inherites li tag as child element of ul.
+  var pText = document.createElement("p");
+  li.appendChild(pText);
+  pText.textContent = addInput.value;
 
-  var pText = document.createElement("p"); //  Created an instance of the element for the specified tag.
-  li.appendChild(pText); // Inherites li tag as child element of ul.
-
-  for (let i = 0; i < noteList.length; i++) {
-    // Refreshing the noteList Basically.
-    pText.textContent = noteList[i];
-  }
-  //==================[ Cloning HTML li through JavaScript ]=====================
+  //===[ Cloning HTML li through JavaScript ]===
 
   var pButtons = document.createElement("p");
   li.appendChild(pButtons);
 
   var editNote = document.createElement("span");
-  editNote.textContent = "edit_note";
   pButtons.appendChild(editNote);
-  editNote.className = "material-symbols-rounded edit-notes";
+  var editImg = document.createElement("img");
+  editImg.setAttribute("src", "images/editNote.png");
+  editNote.appendChild(editImg);
 
   var deleteNote = document.createElement("span");
-  deleteNote.textContent = "close";
   pButtons.appendChild(deleteNote);
-  deleteNote.className = "material-symbols-rounded close";
+  var delImg = document.createElement("img");
+  delImg.className = "closeImg";
+  delImg.setAttribute("src", "images/closeNote.png");
+  deleteNote.appendChild(delImg);
 
   var inputField = document.createElement("input");
   li.appendChild(inputField);
 
-  deleteNote.addEventListener("click", function (e) {
-    e.preventDefault();
-    noteList.pop();
-    console.log("clicked");
-    var del_li = this.parentNode.parentNode;
-    ul.removeChild(del_li);
+  var pin = document.createElement("img");
+  pin.setAttribute("src", "images/pin.png");
+  pin.className = "pin";
+  li.appendChild(pin);
+
+  // ===[ Delelte ]===
+  deleteNote.addEventListener("click", function () {
+    this.parentNode.parentNode.parentNode.removeChild(
+      this.parentNode.parentNode
+    );
   });
-  addInput.value = "";
 
-  //==================[ Cloning HTML li through JavaScript End ]=====================
-
+  // ===[ Edit ]===
   editNote.onclick = function () {
-    inputField.style.display = "block";
-    inputField.focus();
-  };
+    this.parentNode.style.display = "none";
+    this.parentNode.parentNode.children[2].style.display = "block";
+    this.parentNode.parentNode.children[2].value =
+      this.parentElement.parentElement.children[0].innerHTML;
+    this.parentNode.parentNode.children[2].focus();
 
-  inputField.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      console.log(li.firstElementChild);
-      li.firstElementChild.textContent = inputField.value;
-      inputField.value = "";
-      inputField.style.display = "none";
-    }
-  });
+    // ===[ Edit Update ]===
+    this.parentNode.parentNode.children[2].addEventListener(
+      "keypress",
+      function (e) {
+        if (e.key === "Enter") {
+          this.parentNode.children[0].innerHTML =
+            this.parentNode.children[2].value;
+          this.parentNode.children[1].style.display = "block";
+          this.parentNode.children[2].style.display = "none";
+        }
+      }
+    );
+  };
+  addInput.value = "";
 });
 
-function hideNote() {
-  var checked = document.getElementById("hide");
-  var hideLabel = document.querySelector("#hide-list label");
-
-  if (checked.checked == false) {
-    ul.style.display = "none";
-    hideLabel.innerHTML = "Show note";
-  } else {
-    ul.style.display = "block";
-    hideLabel.innerHTML = "Hide note";
-  }
+// ===[ Delelte ]===
+for (var i = 0; i < deleteNote.length; i++) {
+  deleteNote[i].addEventListener("click", function () {
+    this.parentNode.parentNode.parentNode.removeChild(
+      this.parentNode.parentNode
+    );
+  });
 }
 
-// ===[ Theme Panel ]===
+// ===[ Edit ]===
+for (var i = 0; i < editNote.length; i++) {
+  editNote[i].onclick = function () {
+    this.parentNode.style.display = "none";
+    this.parentNode.parentNode.children[2].style.display = "block";
+    this.parentNode.parentNode.children[2].value =
+      this.parentElement.parentElement.children[0].innerHTML;
+    this.parentNode.parentNode.children[2].focus();
 
-var themePanel = document.querySelector("body .logo");
+    // ===[ Edit Update ]===
+    this.parentNode.parentNode.children[2].addEventListener(
+      "keypress",
+      function (e) {
+        if (e.key === "Enter") {
+          this.parentNode.children[0].innerHTML =
+            this.parentNode.children[2].value;
+          this.parentNode.children[1].style.display = "block";
+          this.parentNode.children[2].style.display = "none";
+        }
+      }
+    );
+  };
+}
 
-themePanel.onclick = function () {
-  console.log(document.body.style.setProperty("--primary", "#E32636"));
-  document.body.style.setProperty("--font-color", "#65000B");
-  document.body.style.setProperty("--font-family", '"Pacifico"');
-};
+// ===[ Hide ]===
+hide.addEventListener("click", function () {
+  if (this.parentNode.children[1].checked == false) {
+    this.parentNode.parentNode.children[1].children[1].style.display = "none";
+    this.parentNode.children[0].children[0].setAttribute(
+      "src",
+      "images/down.png"
+    );
+  } else {
+    this.parentNode.parentNode.children[1].children[1].style.display = "block";
+    this.parentNode.children[0].children[0].setAttribute(
+      "src",
+      "images/up.png"
+    );
+  }
+});
+
+var themePanel = document.getElementById("panel");
+
+themePanel.addEventListener("click", function (e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    for (
+      var i = 0;
+      i < document.getElementsByClassName("closeImg").length;
+      i++
+    ) {
+      document
+        .getElementsByClassName("closeImg")
+        [i].setAttribute("src", "images/purplewomen.png");
+    }
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    for (
+      var i = 0;
+      i < document.getElementsByClassName("closeImg").length;
+      i++
+    ) {
+      document
+        .getElementsByClassName("closeImg")
+        [i].setAttribute("src", "images/closeNote.png");
+    }
+  }
+});
